6c227b764f730486d769ede3459eff544b64e1fc
Aishwarya Salian <aishwaryasalian11@gmail.com>
2018-02-12 14:47:50 -0600

    final

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ from flask import render_template
 from flask import request
 import mysql.connector
 from mysql.connector import errorcode
-import pyodbc
-import pymysql
+#import pyodbc
+#import pymysql
+import base64
+import datetime
+import time
+#import uuid
 
+#import pymysql
+from flask import Flask, render_template, session, request, flash, redirect, url_for
 
 # Obtain connection string information from the portal
 #config = {
@@ -22,79 +28,206 @@ database ='AishDb'
 #driver= '{ODBC Driver 13 for SQL Server}'
 #cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
 #}
-cnx = mysql.connector.connect(user="aishdblogin@aishdbserver", password="Qwerty123", host="aishdbserver.mysql.database.azure.com", port=3306, database="apsdb")
-#print cnx
-#print "connection successful"
-# Construct connection string
+db = mysql.connector.connect(user="aishdblogin@aishdbserver", password="Qwerty123", host="aishdbserver.mysql.database.azure.com", port=3306, database="apsdb")
+cursor = db.cursor()
+cursor1 = db.cursor()
+cursor2 = db.cursor()
+#print (db)
+#print ("connection successful")
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
+
+ts = time.time()
+timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+uid=100
+userid=100
+uname="aish"
 
 
-@app.route('/')
-def index():
-    #print "Hello"
-    return render_template('hellouser.html')
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/login.html')
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        uname = request.form['Username']
+        sql = "select first_name, last_name from user where user_name = '" + uname + "'"
+        #print (sql)
+        cursor.execute(sql)
+        #print(cursor.rowcount)
+        results = cursor.fetchall()
+        if cursor.rowcount == 1:
 
-@app.route('/loginPage', methods=['get', 'post'])
-def loginPage():
+            #print(results)
+            for row in results:
+
+                return render_template('uploadFiles.html', username=uname, fname=row[0])
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+@app.route('/registerPage', methods=['POST', 'GET'])
+def registerPage():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+        # check if user is already in session
+
+
+
+        uname = request.form['Username']
+        fname = request.form['Firstname']
+        lname = request.form['Lastname']
+        #print(uname)
+        #print(lname)
+        #sql = "select user_name from user where user_name='" + uname + "'"
+        #print(sql)
+        #cursor.execute(sql)
+        #res=cursor.fetchall();
+        #print(res)
+        if uname == '' or fname == '' or lname == '':
+            flash('Fields cannot be empty')
+            return render_template('register.html')
+
+        sql = "insert into user values ('" + uname + "','" + fname + "','" + lname + "')"
+        #print(sql)
+        cursor.execute(sql)
+        #res = cursor.fetchall()
+        #print(res)
+        db.commit()
+        #db.close()
+        return '<h1>User Registration successful</h1><br><form action="../"><input type="Submit" value="Login to continue"></form>'
+
+
+# logout
+@app.route('/logout', methods=['POST', 'GET'])
+def logout():
+    flash('You have been successfully logged out')
+    return redirect(url_for('login'))
+
+
+@app.route('/createAlbum', methods=['POST', 'GET'])
+def createAlbum():
+
+    if request.method == 'POST':
+        #uname = request.form['Username']
+        #fname = request.form['Firstname']
+        #lname = request.form['Lastname']
+        album_name = request.form['album']
+        sql = "select * from album where user_name = '" + uname + "' and Album_name ='" + album_name + "'"
+        cursor.execute(sql)
+        if cursor.rowcount == 0:
+            try:
+                sql = "INSERT INTO album (user_name, Album_name, Album_creation_date) values ('" + uname + "','" + album_name + "','" + timestamp + "')"
+                cursor.execute(sql)
+                db.commit()
+                return render_template('upload.html')
+            except:
+                db.rollback()
+        else:
+            return '<h1>Album ' + album_name + ' already exists on cloud. Please provide some other album name. </h1><br><form"><input type="button" value="Lets go back" onclick="history.go(-1)"></form>'
+    else:
+        return render_template('login.html')
+
+
+def allowed_file(filename):
+
+    #print(filename.rsplit('.', 1)[1].lower())
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploadFiles', methods=['POST', 'GET'])
+@app.route('/uploadFiles.html', methods=['POST', 'GET'])
+def uploadFiles():
+    if request.method == 'POST':
+        file_name = request.files['file_upload']
+        file_title = request.form['title']
+        filename = file_name.filename
+        #print(filename)
+        sql = "select * from Image where user_name = '" + uname + "' and Image_name ='" + filename + "'"
+        #print(sql)
+        cursor.execute(sql)
+        #print(cursor.rowcount)
+        if cursor.rowcount  <=0 and allowed_file(filename):
+            file_contents = file_name.read()
+            #print(file_contents)
+            if len(file_contents) > 1024 * 1024:
+                return '<h1>File size greater than 1MB</h1><br><form action="../"><input type="Submit" value="Lets go back"></form>'
+            sql = "INSERT INTO Image (user_name, Title, Image_name, Data, Date_created) VALUES('" + uname + "','" + file_title + "','" + filename + "','" + base64.standard_b64encode(file_contents) + "','" + timestamp + "')"
+            #print(sql)
+            cursor.execute(sql)
+
+            db.commit()
+
+
+        elif cursor.rowcount > 0:
+            return '<h1>File ' + filename + '  exists on cloud.  upload other file.</h1><br><form"><input type="button" value="Lets go back" onclick="history.go(-1)"></form>'
+        else:
+            return '<h1>Incorrect file extension</h1><br><form"><input type="button" value="Lets go back" onclick="history.go(-1)"></form>'
+        return '<h1>Files have been uploaded<h1><br><form"><input type="button" value="Lets go back" onclick="history.go(-1)"></form>'
     return render_template('login.html')
 
 
-@app.route('/uploadPage', methods=['get', 'post'])
-def uploadPage():
-    #print "Hello"
-    return render_template('upload.html')
+@app.route('/viewPhotos', methods=['POST', 'GET'])
+def viewPhotos():
+    if request.method == 'POST':
+        photos_array = []
+        sql2 = "select Image_ID, Data, Title, Date_Created from Image where Image_ID not in (select Image_ID from rating)"
+        #print(sql2)
+        cursor2.execute(sql2)
+        #print(cursor2.rowcount)
+        if cursor2.rowcount > 0:
+            for ind2 in cursor2:
+                photos_array.append([ind2[0], ind2[1], ind2[2], ind2[3], 0])
 
-@app.route('/uploadImage', methods=['get', 'post'])
-def upload1():
-    f = request.files['upload_files']
-    file_name = f.filename
-    comm = request.form['comments']
-    print (file_name)
-    newfile = "D:/Cloud_Assignments/Assignment10_Azure/" + file_name
-    print (newfile)
-    # block_blob_service.create_blob_from_path('saipriya', file_name, newfile,
-    #                                          content_settings=ContentSettings(content_type='image/png'))
-    # imgUrl = 'https://mycloudassign.blob.core.windows.net/saipriya/' + file_name
-    # insertQuery = "insert into Photo (img) values ('%s')" % (imgUrl)
-    # #print insertQuery
-    # cur = myConnection.cursor()
-    # cur.execute(insertQuery)
-    # query1 = 'select img from Photo'
-    # cur.execute(query1)
-    # res = cur.fetchall()
-    # myConnection.commit()
-    return 'File uploaded successfully'
-
-
-@app.route('/viewImages' , methods=['get', 'post'])
-def viewImages():
- return render_template('list.html')
-
-@app.route('/list', methods=['get', 'post'])
-def list():
-    # list=[]
-    # generator = block_blob_service.list_blobs('saipriya')
-    # for blob in generator:
-    #     print(blob.name)
-    #     list.append("https://mycloudassign.blob.core.windows.net/saipriya/" + blob.name)
-    # print list[1]
-    return render_template('display.html', img=list)
+        sql = "select Image_ID,Data,Title,Date_Created from Image order by Image_ID desc"
+        #print(sql)
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        #print(res)
+        # photos_array = []
+        for ind in cursor:
+            sql1 = "select image_id, avg(rating) from rating group by %d" % (1)
+            cursor1.execute(sql1)
+            for ind1 in cursor1:
+                if ind1[1] != 0 and ind[0] == ind1[0]:
+                    photos_array.append([ind[0], ind[1], ind[2], ind[3], ind1[1]])
+                elif ind1[1] == 0:
+                    photos_array.append([ind[0], ind[1], ind[2], ind[3], 0])
+        # print photos_array
+        return render_template('viewphotos.html', photos=photos_array)
 
 
-@app.route('/show', methods=["GET"])
-def showUserData():
-    imageList = []
-    query = "SELECT * FROM Photo where photo_id=23"
-    # cur = myConnection.cursor()
-    # cur.execute(query)
-    # result = cur.fetchall()
-    # for row in result:
-    #     image = b64encode(row[3])
-    #     imageList.append(image)
-    #print datetime.now()
-    return render_template("display.html", image=imageList)
+@app.route('/giveRatings/<image_id>', methods=['POST', 'GET'])
+def giveRatings(image_id):
+
+    if request.method == 'POST':
+        rating = request.form['rating']
+        comments = request.form['comments']
+        sql = "select * from rating where user_name ='" + uname + "' and image_id ='" + image_id + "'"
+        cursor.execute(sql)
+        if cursor.rowcount == 0:
+            try:
+                sql = "insert into rating (user_name,image_id,rating,comments) values('" + uname + "','" + image_id + "','" + rating + "','" + comments + "')"
+                cursor.execute(sql)
+                db.commit()
+                return '<h1>Rating submitted!</h1><br><form><input type="button" value="Lets go back" onclick = "history.go(-2)"></form>'
+            except:
+                db.rollback()
+        else:
+            try:
+                sql = "update rating set rating ='" + rating + "', comments='" + comments + "' where uid ='" + userid + "' and image_id='" + image_id + "'"
+                cursor.execute(sql)
+                db.commit()
+                return '<h1>Rating updated!</h1><br><form><input type="button" value="Lets go back" onclick = "history.go(-2)"></form>'
+            except:
+                db.rollback()
+    return render_template('giveRatings.html', image_id=image_id)
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
---
+++ mainOld.py
@@ -0,0 +1,100 @@
+#import PIL
+
+#from PIL import Image
+#import cStringIO
+from flask import Flask
+#from azure.storage.blob import BlockBlobService
+#from azure.storage.blob import ContentSettings
+from flask import render_template
+from flask import request
+import mysql.connector
+from mysql.connector import errorcode
+import pyodbc
+import pymysql
+
+
+# Obtain connection string information from the portal
+#config = {
+server ='aishdb.database.windows.net'
+username ='aish'
+password ='Qwerty123'
+database ='AishDb'
+#driver= '{ODBC Driver 13 for SQL Server}'
+#cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+#}
+cnx = mysql.connector.connect(user="aishdblogin@aishdbserver", password="Qwerty123", host="aishdbserver.mysql.database.azure.com", port=3306, database="apsdb")
+print (cnx)
+print ("connection successful")
+# Construct connection string
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    #print "Hello"
+    return render_template('hellouser.html')
+
+@app.route('/loginPage', methods=['get', 'post'])
+def loginPage():
+    return render_template('login.html')
+
+
+@app.route('/uploadPage', methods=['get', 'post'])
+def uploadPage():
+    #print "Hello"
+    return render_template('upload.html')
+
+@app.route('/uploadImage', methods=['get', 'post'])
+def upload1():
+    f = request.files['upload_files']
+    file_name = f.filename
+    comm = request.form['comments']
+    print (file_name)
+    newfile = "D:/Cloud_Assignments/Assignment10_Azure/" + file_name
+    print (newfile)
+    # block_blob_service.create_blob_from_path('saipriya', file_name, newfile,
+    #                                          content_settings=ContentSettings(content_type='image/png'))
+    # imgUrl = 'https://mycloudassign.blob.core.windows.net/saipriya/' + file_name
+    # insertQuery = "insert into Photo (img) values ('%s')" % (imgUrl)
+    # #print insertQuery
+    # cur = myConnection.cursor()
+    # cur.execute(insertQuery)
+    # query1 = 'select img from Photo'
+    # cur.execute(query1)
+    # res = cur.fetchall()
+    # myConnection.commit()
+    return 'File uploaded successfully'
+
+
+@app.route('/viewImages' , methods=['get', 'post'])
+def viewImages():
+ return render_template('list.html')
+
+@app.route('/list', methods=['get', 'post'])
+def list():
+    # list=[]
+    # generator = block_blob_service.list_blobs('saipriya')
+    # for blob in generator:
+    #     print(blob.name)
+    #     list.append("https://mycloudassign.blob.core.windows.net/saipriya/" + blob.name)
+    # print list[1]
+    return render_template('display.html', img=list)
+
+
+@app.route('/show', methods=["GET"])
+def showUserData():
+    imageList = []
+    query = "SELECT * FROM Photo where photo_id=23"
+    # cur = myConnection.cursor()
+    # cur.execute(query)
+    # result = cur.fetchall()
+    # for row in result:
+    #     image = b64encode(row[3])
+    #     imageList.append(image)
+    #print datetime.now()
+    return render_template("display.html", image=imageList)
+
+
+if __name__ == '__main__':
+    app.run()
